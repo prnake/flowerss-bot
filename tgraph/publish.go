bb9a3de44b79ae9079763993ecf99cbfce0aa4b6
papersnake <prnake@gmail.com>
2020-07-31 14:07:52 +0800

    beautify

--- tgraph/publish.go
+++ tgraph/publish.go
@@ -2,24 +2,22 @@ package tgraph
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"math/rand"
 	"time"
 )
 
-func PublishHtml(sourceTitle string, title string, rawLink string, htmlContent string) (string, error) {
+func PublishHtml(sourceTitle string, title string, rawLink string, html string) (string, error) {
 	//html = fmt.Sprintf(
-	//	"<p>本文章由 <a href=\"https://github.com/indes/flowerss-bot\">flowerss</a> 抓取自RSS，版权归<a href=\"\">源站点</a>所有。</p><hr>",
+	//      "<p>本文章由 <a href=\"https://github.com/indes/flowerss-bot\">flowerss</a> 抓取自RSS，版权归<a href=\"\">源站点</a>所有。</p><hr>",r := p
 	//) + html + fmt.Sprintf(
-	//	"<hr><p>本文章由 <a href=\"https://github.com/indes/flowerss-bot\">flowerss</a> 抓取自RSS，版权归<a href=\"\">源站点</a>所有。</p><p>查看原文：<a href=\"%s\">%s - %s</p>",
-	//	rawLink,
-	//	title,
-	//	sourceTitle,
+	//      "<hr><p>本文章由 <a href=\"https://github.com/indes/flowerss-bot\">flowerss</a> 抓取自RSS，版权归<a href=\"\">源站点</a>所有。</p><p>查看原文：<a href=\"%s\">%s - %s</p>",
+	//      rawLink,
+	//      title,
+	//      sourceTitle,
 	//)
-
-	htmlContent = html.UnescapeString(htmlContent) + fmt.Sprintf(
-		"<hr><p>本文章由 <a href=\"https://github.com/indes/flowerss-bot\">flowerss</a> 抓取自RSS，版权归<a href=\"\">源站点</a>所有。</p><p>查看原文：<a href=\"%s\">%s - %s</p>",
+	html = html + fmt.Sprintf(
+		"<hr><p>查看原文：<a href=\"%s\">%s - %s</p>",
 		rawLink,
 		title,
 		sourceTitle,
@@ -27,7 +25,7 @@ func PublishHtml(sourceTitle string, title string, rawLink string, htmlContent s
 	rand.Seed(time.Now().Unix()) // initialize global pseudo random generator
 	client := clientPool[rand.Intn(len(clientPool))]
 
-	if page, err := client.CreatePageWithHTML(title+" - "+sourceTitle, sourceTitle, rawLink, htmlContent, true); err == nil {
+	if page, err := client.CreatePageWithHTML(title+" - "+sourceTitle, sourceTitle, rawLink, html, true); err == nil {
 		log.Printf("Created telegraph page url: %s", page.URL)
 		return page.URL, err
 	} else {
